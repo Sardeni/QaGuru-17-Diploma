2fd77b0a73ddef7c029af82529d7a871a2c20b1e
Sardeni <nordisnsk@gmail.com>
2023-03-03 12:57:46 +0400

    save commit

--- src/test/java/ru/tinkoff/tests/web/CreditCardTests.java
+++ src/test/java/ru/tinkoff/tests/web/CreditCardTests.java
@@ -13,7 +13,7 @@ import static io.qameta.allure.SeverityLevel.NORMAL;
 
 public class CreditCardTests extends TestBase {
 
-    @ParameterizedTest(name = "Checking сredit value {1} after choosing slider {0}")
+    @ParameterizedTest(name = "Checking сredit value: {1} after choosing slider: {0}")
     @CsvSource(value = {
             "15000, 15 000 ₽",
             "100000, 100 000 ₽",
@@ -24,7 +24,7 @@ public class CreditCardTests extends TestBase {
     @Owner("emelianovam")
     @Severity(NORMAL)
     @Feature("Credit card form")
-    public void checkingHeadersParametrized(String value, String inputValue) {
+   public void checkingHeadersParametrized(String value, String inputValue) {
         open("/cards/credit-cards/");
         $("[data-qa-type=\"uikit/slider.point\"][data-value=\"" + value + "\"]").click();
         creditCardsPage.checkElementAttribute(creditCardsPage.sliderInput, "value", inputValue);
